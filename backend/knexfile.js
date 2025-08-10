5d757c9ea2a69b30b890f0e09249275ce5e86a72
Divakar <divakar07n@gmail.com>
2025-08-10 21:27:07 +0530

    fix error 4

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -1,18 +1,32 @@
 require('dotenv').config();
 
-const knexConfig = {
-  client: 'mysql2',
-  connection: {
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    port:process.env.PORT,
-    ssl: { rejectUnauthorized: false }, // important for Aiven SSL
-  },
+module.exports = {
+  development: {
+    client: 'mysql2',
+    connection: process.env.DATABASE_URL,
+    migrations: {
+      directory: './migrations'
+    },
+    seeds: {
+      directory: './seeds'
+    }
+  }
 };
 
-const knex = require('knex')(knexConfig);
-const { Model } = require('objection');
-Model.knex(knex);
+// require('dotenv').config();
+
+// const knexConfig = {
+//   client: 'mysql2',
+//   connection: {
+//     host: process.env.DB_HOST,
+//     user: process.env.DB_USER,
+//     password: process.env.DB_PASSWORD,
+//     database: process.env.DB_NAME,
+//     ssl: { rejectUnauthorized: false }, // important for Aiven SSL
+//   },
+// };
+
+// const knex = require('knex')(knexConfig);
+// const { Model } = require('objection');
+// Model.knex(knex);
 
