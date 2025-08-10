45a41700c68ac6d7c969c5eb4c628146d8ea1a25
Divakar <divakar07n@gmail.com>
2025-08-10 20:39:14 +0530

    fix error

--- backend/models/Job.js
+++ backend/models/Job.js
@@ -7,3 +7,17 @@ class Job extends Model {
 }
 
 module.exports = Job;
+
+
+// const { Model } = require('objection');
+// const knex = require('../db/knex'); // your knex instance
+
+// Model.knex(knex);
+
+// class Job extends Model {
+//   static get tableName() {
+//     return 'jobs';  // your actual table name in MySQL
+//   }
+// }
+
+// module.exports = Job;
